763872fbc77824e7ec01c98c30b6670d5078e78d
mixpapp <mixpapp1998@gmail.com>
2024-05-11 00:51:40 +0300

    commit3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
-
+import { EventBindComponent } from './components/event-bind/event-bind.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent],
+  imports: [PersonTableComponent, EventBindComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,5 +13,18 @@ export class AppComponent {
   name = "mikepapp";
 
 
- 
+  person0 = {
+  
+    givenName : "Mike",
+    surName : "Pappas",
+    age : 30,
+    email: 'gg@yahoo.gr',
+    address: "skiph 13"
+
+    
+
+  
+};
 }
+
+
---
+++ src/app/components/event-bind/event-bind.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-bind',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-bind.component.html',
+  styleUrl: './event-bind.component.css'
+})
+export class EventBindComponent {
+
+}
